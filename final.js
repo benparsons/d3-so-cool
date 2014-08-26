f7db629d248a901ebbfff669ddb70310c93dae05
Ben Parsons <ben+github@bpulse.co.uk>
2014-08-26 22:17:45 +0100

    final working version

--- final.js
+++ final.js
@@ -1,25 +1,35 @@
-var svg = d3.select("svg");
 
-var data = tributary.pics.data.children
-//.sort(function (a,b) {return a.data.score  - b.data.score});
-
-var bars = g.selectAll("rect")
-.data(data);
-
-bars.enter().append("rect");
-
-var maxScore = d3.max(data, function(d) {return d.data.score;});
-var yScale = d3.scale.linear()
-.domain([0, maxScore])
-.range([500,0]);
-
-
-bars.attr({
-  x: function(d,i) {return i * 15},
-  y: function(d) {return 500-yScale(d.data.score)} ,
-  width: 10,
-  height: function (d) {return yScale( d.data.score)}
+var data;
+d3.json("pics.json", function(error, json) {
+  if (error) return console.warn(error);
+  data = json.data.children;
+  //data = data.sort(function (a,b) {return a.data.score  - b.data.score});
+  visualizeit();
 });
+
+function visualizeit() {
+    var svg = d3.select("svg");
+    var bars = svg.selectAll("rect")
+    .data(data);
+
+    bars.enter().append("rect");
+    console.log(data);
+
+    var maxScore = d3.max(data, function(d) {return d.data.score;});
+    var yScale = d3.scale.linear()
+    .domain([0, maxScore])
+    .range([500,0]);
+
+
+    bars.attr({
+      x: function(d,i) {return i * 15},
+      y: function(d) {return 500-yScale(d.data.score)} ,
+      width: 10,
+      height: function (d) {return yScale( d.data.score)}
+    });
+}
+
+
 
 /*
 var maxScore = d3.max(data, function(d) {return d.data.score;});
