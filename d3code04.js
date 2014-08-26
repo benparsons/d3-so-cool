e25d484684848228fd322c053602e28d3ff09367
Ben Parsons <ben+github@bpulse.co.uk>
2014-08-26 23:49:27 +0100

    improvements

--- d3code04.js
+++ d3code04.js
@@ -12,19 +12,13 @@ function visualizeit() {
 
     bars.enter().append("rect");
     console.log(data);
-/*
-    var maxScore = d3.max(data, function(d) {return d.data.score;});
-    var yScale = d3.scale.linear()
-    .domain([0, maxScore])
-    .range([0,500]);
-*/
+
 
     bars.attr({
       x: function(d,i) {return i * 15},
       y: 0 ,
       width: 10,
       height: 100,
-      //height: function (d) {return yScale( d.data.score)}
       height: function (d) {return d.data.score}
     });
 }
